477f6acfd558c7f4b73327dedc68a717e5e2a103
Chinh DT <chinhdtth2009021@fpt.edu.vn>
2021-09-02 09:07:34 +0700

    template

---
+++ resources/views/Admin/layout/index.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>@yield('title')Dashboard - Mazer Admin Dashboard</title>
+    @include('.Admin.layout.link')
+</head>
+
+<body>
+<div id="app">
+  @include('.Admin.layout.menu')
+    <div id="main">
+       @include('.Admin.layout.header')
+        @yield('content')
+        @include('.Admin.layout.footer')
+    </div>
+</div>
+@include('Admin.layout.script')
+@yield('script')
+</body>
+
+</html>
---
+++ resources/views/Client/layout/footer.blade.php
@@ -0,0 +1,106 @@
+<!-- ::::::  Start  Footer ::::::  -->
+<footer class="footer m-t-100">
+    <div class="container">
+        <!-- Start Footer Top Section -->
+        <div class="footer__top">
+            <div class="row">
+                <div class="col-lg-4 col-md-5">
+                    <div class="footer__about">
+                        <div class="footer__logo">
+                            <a href="index.html" class="footer__logo-link">
+                                <img src="assets/img/logo/logo.png" alt="" class="footer__logo-img">
+                            </a>
+                        </div>
+                        <ul class="footer__address">
+                            <li class="footer__address-item"><i class="fa fa-home"></i>No: 58 A, your Street, Baltimore, USA 4508</li>
+                            <li class="footer__address-item"><i class="fa fa-phone-alt"></i>+01 23456789</li>
+                            <li class="footer__address-item"><i class="fa fa-envelope"></i>support@somemail.com</li>
+                        </ul>
+                        <ul class="footer__social-nav">
+                            <li class="footer__social-list"><a href="#" class="footer__social-link"><i class="fab fa-facebook-f"></i></a></li>
+                            <li class="footer__social-list"><a href="#" class="footer__social-link"><i class="fab fa-twitter"></i></a></li>
+                            <li class="footer__social-list"><a href="#" class="footer__social-link"><i class="fab fa-youtube"></i></a></li>
+                            <li class="footer__social-list"><a href="#" class="footer__social-link"><i class="fab fa-google-plus-g"></i></a></li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-lg-2 col-md-3 col-sm-6 col-12">
+                    <!-- Start Footer Nav -->
+                    <div class="footer__menu">
+                        <h4 class="footer__nav-title footer__nav-title--dash footer__nav-title--dash-red">INFORMATION</h4>
+                        <ul class="footer__nav">
+                            <li class="footer__list"><a href="" class="footer__link">Delivery Information</a></li>
+                            <li class="footer__list"><a href="" class="footer__link">Advanced Search</a></li>
+                            <li class="footer__list"><a href="" class="footer__link">Helps & Faqs</a></li>
+                            <li class="footer__list"><a href="" class="footer__link">Store Location</a></li>
+                            <li class="footer__list"><a href="" class="footer__link">Orders & Returns</a></li>
+                            <li class="footer__list"><a href="" class="footer__link">Deliveries</a></li>
+                            <li class="footer__list"><a href="" class="footer__link"> Refund & Returns</a></li>
+                        </ul>
+                    </div> <!-- End Footer Nav -->
+                </div>
+                <div class="col-lg-2 col-md-3 col-sm-6 col-12">
+                    <div class="footer__menu">
+                        <h4 class="footer__nav-title footer__nav-title--dash footer__nav-title--dash-red">OUR COMPANY</h4>
+                        <ul class="footer__nav">
+                            <li class="footer__list"><a href="" class="footer__link">Delivery</a></li>
+                            <li class="footer__list"><a href="" class="footer__link">Legal Notice</a></li>
+                            <li class="footer__list"><a href="" class="footer__link">Sitemap</a></li>
+                            <li class="footer__list"><a href="" class="footer__link">Secure payment</a></li>
+                            <li class="footer__list"><a href="" class="footer__link">Blog</a></li>
+                            <li class="footer__list"><a href="" class="footer__link">About us</a></li>
+                            <li class="footer__list"><a href="" class="footer__link">Carrers</a></li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-lg-2 col-md-3 col-sm-6 col-12">
+                    <div class="footer__menu">
+                        <h4 class="footer__nav-title footer__nav-title--dash footer__nav-title--dash-red">MY ACCOUNT</h4>
+                        <ul class="footer__nav">
+                            <li class="footer__list"><a href="" class="footer__link">Search Terms</a></li>
+                            <li class="footer__list"><a href="" class="footer__link">Advanced Search</a></li>
+                            <li class="footer__list"><a href="" class="footer__link">Helps & Faqs</a></li>
+                            <li class="footer__list"><a href="" class="footer__link">Store Location</a></li>
+                            <li class="footer__list"><a href="" class="footer__link">Orders & Returns</a></li>
+                            <li class="footer__list"><a href="" class="footer__link">Deliveries</a></li>
+                            <li class="footer__list"><a href="" class="footer__link">Refund & Returns</a></li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-lg-2 col-md-3 col-sm-6 col-12">
+                    <div class="footer__menu">
+                        <h4 class="footer__nav-title footer__nav-title--dash footer__nav-title--dash-red">OPENING TIME</h4>
+                        <ul class="footer__nav">
+                            <li class="footer__list">Mon - Fri: 8AM - 10PM</li>
+                            <li class="footer__list">Sat: 9AM-8PM</li>
+                            <li class="footer__list">Suns: 14hPM-18hPM</li>
+                            <li class="footer__list">Mon - Fri: 8AM - 10PM</li>
+                            <li class="footer__list">We Work All The Holidays</li>
+                            <li class="footer__list"><a href="" class="footer__link font--bold">Download our app</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div> <!-- End Footer Top Section -->
+        <!-- Start Footer Bottom Section -->
+        <div class="footer__bottom">
+            <div class="row">
+                <div class="col-lg-8 col-md-6 col-12">
+                    <!-- Start Footer Copyright Text -->
+                    <div class="footer__copyright-text">
+                        <p>Copyright &copy; <a href="https://hasthemes.com/">HasThemes</a>. All Rights Reserved</p>
+                    </div> <!-- End Footer Copyright Text -->
+                </div>
+                <div class="col-lg-4 col-md-6 col-12">
+                    <!-- Start Footer Payment Logo -->
+                    <div class="footer__payment">
+                        <a href="#" class="footer__payment-link">
+                            <img src="assets/img/company-logo/payment.png" alt="" class="footer__payment-img">
+                        </a>
+                    </div>  <!-- End Footer Payment Logo -->
+                </div>
+            </div>
+        </div> <!-- End Footer Bottom Section -->
+    </div>
+</footer> <!-- ::::::  End  Footer ::::::  -->
+
