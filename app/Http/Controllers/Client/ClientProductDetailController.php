b08fda272693fa4d5ab21174d293f065b5078bb9
bui-hoang-02 <gangtergamingx@gmail.com>
2021-09-15 09:56:12 +0700

    hiển thị dữ liệu ra client

---
+++ app/Http/Controllers/Client/ClientProductDetailController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers\Client;
+
+use App\Http\Controllers\Controller;
+use App\Models\Product;
+use Illuminate\Http\Request;
+
+class ClientProductDetailController extends Controller
+{
+    public function list()
+    {
+        return "";
+    }
+    public function detail($id)
+    {
+        $product = Product::find($id);
+        return view('Client.product-detail', [
+            'product' => $product
+        ]);
+    }
+
+}
